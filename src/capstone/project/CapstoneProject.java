2498d8e502f7bff74defed00acf85fc3ea43aebb
steff-petro <85114094+steff-petro@users.noreply.github.com>
2022-06-09 12:56:47 +0200

    Added jar files to libraries

--- src/capstone/project/CapstoneProject.java
+++ src/capstone/project/CapstoneProject.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+delete this class once Emma adds ui main class
  */
 package capstone.project;
 
