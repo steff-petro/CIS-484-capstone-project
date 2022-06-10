6d9b614b82c3acf0ddb18180c255680255ca6db9
steff-petro <85114094+steff-petro@users.noreply.github.com>
2022-06-10 18:12:58 +0200

    Updating github

--- src/capstone/project/Animal.java
+++ src/capstone/project/Animal.java
@@ -1,9 +1,77 @@
 /*
-
+This is the Animal class
  */
 package capstone.project;
 
+// Imports
+
+
 
 public class Animal {
-    
+
+    // Class variables and class array list
+    private String animalId;
+    private String animalName;
+    private String animalType;
+    private int animalAge;
+
+    // Default Constructor
+    public Animal() {
+
+        this.animalId = "Default Animal ID";
+        this.animalName = "Default Animal Name";
+        this.animalType = "Default Animal Type";
+        this.animalAge = 0;
+
+    }
+
+    // Overriden constructor
+   public Animal(String animalId, String animalName, String animalType, int animalAge) {
+        setAnimalId(animalId);
+        setAnimalName(animalName);
+        setAnimalType(animalType);
+        setAnimalAge(animalAge);
+
+    }
+
+    // @return the animalId
+    public String getAnimalId() {
+        return animalId;
+    }
+
+    // @param animalId the animalID to set
+    public void setAnimalId(String animalId) {
+        this.animalId = animalId;
+    }
+
+    // @return the animalName
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    // @param animalName the animalName to set
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    // @return the animalType
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    // @param animalType the animalType to set
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    // @return the animalAge
+    public int getAnimalAge() {
+        return animalAge;
+    }
+
+    // @param animalAge the animalAge to set
+    public void setAnimalAge(int animalAge) {
+        this.animalAge = animalAge;
+    }
+
 }
--- src/capstone/project/Volunteer.java
+++ src/capstone/project/Volunteer.java
@@ -1,9 +1,188 @@
 /*
-
+this is the Volunteer class
  */
 package capstone.project;
 
-
+// Imports
 public class Volunteer {
+
+    // Class variables
+    private String volunteerId;
+    private String firstName;
+    private String lastName;
+    private String volunteerStreet;
+    private String volunteerCity;
+    private String volunteerState;
+    private int volunteerZip;
+    private String email;
+    private String phoneNumber;
+    private String experience;
+    private String status;
+    private String password;
+    private String specialization;
+
+    // Default constructor
+    public Volunteer() {
+        
+        this.volunteerId = "Default Volunteer ID";
+        this.firstName = "Default Volunteer First Name";
+        this.lastName = "Defualt Volunteer Last Name";
+        
+        
+        
+        
+        
+    }
+
+
+// Overriden constructor
+    public Volunteer(String volunteerId, String firstName, String lastName, String volunteerStreet, String volunteerCity, String volunteerState, int volunteerZip, String email, String phoneNumber, String experience, String status, String password, String specialization) {
+        setVolunteerId(volunteerId);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setVolunteerStreet(volunteerStreet);
+        setVolunteerCity(volunteerCity);
+        setVolunteerState(volunteerState);
+        setVolunteerZip(volunteerZip);
+        setEmail(email);
+        setPhoneNumber(phoneNumber);
+        setExperience(experience);
+        setStatus(status);
+        setPassword(password);
+        setSpecialization(specialization);
+    }
+
+    // @return the volunteerId
+    public String getVolunteerId() {
+        return volunteerId;
+    }
+
+    // @param volunteerId the volunteerId to set
+    public void setVolunteerId(String volunteerId) {
+        this.volunteerId = volunteerId;
+    }
+
+    // @return the firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // @param firstName the firstName to set
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // @return the lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    // @param lastName the lastName to set
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // @return the volunteerStreet
+    public String getVolunteerStreet() {
+        return volunteerStreet;
+    }
+
+    // @param volunteerStreet the volunteerStreet to set
+    public void setVolunteerStreet(String volunteerStreet) {
+        this.volunteerStreet = volunteerStreet;
+    }
+
+    // @return the volunteerCity
+    public String getVolunteerCity() {
+        return volunteerCity;
+    }
+
+    // @param volunteerCity the volunteerCity to set
+    public void setVolunteerCity(String volunteerCity) {
+        this.volunteerCity = volunteerCity;
+    }
+
+    // @return the volunteerState
+    public String getVolunteerState() {
+        return volunteerState;
+    }
+
+    // @param volunteerState the volunteerState to set
+    public void setVolunteerState(String volunteerState) {
+        this.volunteerState = volunteerState;
+    }
+
+    // @return the volunteerZip
+    public int getVolunteerZip() {
+        return volunteerZip;
+    }
+
+    // @param volunteerZip the volunteerZip to set
+    public void setVolunteerZip(int volunteerZip) {
+        this.volunteerZip = volunteerZip;
+    }
+
+    // @return the email
+    public String getEmail() {
+        return email;
+    }
+
+    // @param email the email to set
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // @return the phoneNumber
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // @param phoneNumber the phoneNumber to set
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    // @return the experience
+    public String getExperience() {
+        return experience;
+    }
+
+    // @param experience the experience to set
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    // @return the status
+    public String getStatus() {
+        return status;
+    }
+
+    // @param status the status to set
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    // @return the password
+    public String getPassword() {
+        return password;
+    }
+
+    // @param password the password to set
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // @return the specialization
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    // @param specialization the specialization to set
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+    
+    
+    
     
 }
